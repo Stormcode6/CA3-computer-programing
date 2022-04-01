2d270ce2d936536799105bba2f72bed2ec1a7e21
Stormcode6 <98536880+Stormcode6@users.noreply.github.com>
2022-04-01 09:56:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CA3_S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void PlayerStats(string playerName, int score)
+        {
+
+        }
+    }
+}
+
